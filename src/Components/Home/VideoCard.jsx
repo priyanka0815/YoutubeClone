e7ad8b0334422fe10c14a90e96b1762b570bbd70
priyanka0815 <pt290030@gmail.com>
2024-01-25 21:56:50 +0530

    completed home

--- src/Components/Home/VideoCard.jsx
+++ src/Components/Home/VideoCard.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { VerifiedIcon } from '../../utils/Icons';
 
 const VideoCard = (props) => {
-  return <div></div>;
+  return (
+    <div className="videocard">
+      <div className="thumbnail">
+        <Link to={`/watch?v=${props.videoId}`}>
+          <img
+            src={`/images/thumbnails/${props.thumbnail}`}
+            alt={props.videoTitle}
+          />
+          <span className="duration">
+            {props.duration}
+          </span>
+        </Link>
+      </div>
+      <div className="info">
+        <div className="channel-logo">
+          <Link to={`/@${props.channelHandle}`}>
+            <img
+              src={`/images/channels/${props.channelLogo}`}
+              alt={props.channelName}
+            />
+          </Link>
+        </div>
+        <div className="video-info">
+          <h4>
+            <Link
+              to={`/watch?v=${props.videoId}`}
+            >
+              {props.videoTitle}
+            </Link>
+          </h4>
+          <div className="channel-info">
+            <span>
+              <Link
+                to={`/@${props.channelHandle}`}
+              >
+                {props.channelName}
+              </Link>
+            </span>
+            {props.isVerified && (
+              <span className="verify">
+                <VerifiedIcon />
+              </span>
+            )}
+          </div>
+
+          <div className="extra-info">
+            <span>{props.totalViews} views</span>
+            <span>•</span>
+            <span>{props.uploadTime}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoCard;
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -16,9 +16,155 @@ const index = () => {
           channelName="ZeeTv"
           isVerified={true}
           channelHandle="dsgdg"
-          duration="12"
+          duration="12:13"
           totalViews="235"
           uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
+        />
+        <VideoCard
+          thumbnail="1.webp"
+          videoId="watchtv"
+          channelLogo="ZeeTv.jpg"
+          channelName="ZeeTv"
+          isVerified={true}
+          channelHandle="dsgdg"
+          duration="12:13"
+          totalViews="235"
+          uploadTime="2024-01-25T11:15:14.319Z"
+          videoTitle="Abhishek is a winner of BiggBoss 17 and runner up is Munawar Faruqi"
         />
       </div>
     </div>
